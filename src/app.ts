f88f332a933d938b55580974f166a8d2644ae38e
Nitish Singh <nitishsingh925@gmail.com>
2024-09-16 22:24:05 +0530

    base off swagger ui

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import authRouter from "./routes/auth.router";
 import { logger } from "hono/logger";
 import { HTTP_STATUS } from "./utils/httpStatus";
+import { swaggerUI } from "@hono/swagger-ui";
+import * as fs from "fs";
 
 const app = new Hono();
 
@@ -12,6 +14,16 @@ app.get("/", (c) => c.text("Hono! with Bun"));
 
 // Register the auth router
 app.route("/api/v1/auth", authRouter);
+
+// Swagger UI
+
+app.get("/swagger.json", (c) => {
+  const swaggerDoc = fs.readFileSync("./swagger.json", "utf-8");
+  return c.json(JSON.parse(swaggerDoc));
+});
+
+// Swagger UI
+app.get("/api", swaggerUI({ url: "/swagger.json" }));
 
 // Catch-all route for handling 404 errors
 app.use("*", async (c) =>
